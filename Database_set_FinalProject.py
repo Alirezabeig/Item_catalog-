a95e142a8fdd6a919f668104531d0775af00389f
Alireza Beig <43462308+Alirezabeig@users.noreply.github.com>
2019-01-14 20:36:21 -0700

    feature
    Adding users to Category so specific users can edit.

--- Database_set_FinalProject.py
+++ Database_set_FinalProject.py
@@ -23,6 +23,8 @@ class Category(Base):
 
     id= Column(Integer, primary_key=True)
     name= Column(String (30), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
